b3e209f751a4ec4eab3731fa6d0d7598022283db
ArvisCE <ArvisCE@gmail.com>
2023-05-17 18:35:31 +0300

    feat: functionality for deleting, created a box that will let you add new items

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
+  <div id="site">
+    <input type="button" value="+" @click="new_item()">
     <ItemsList :items="itemsData" />
+  </div>
+  <div class="new_item" v-if="new_item_box">
+      <button class="close_button" @click="close_box()">X</button>
+      <center>
+        <input class="input_field" type="text" placeholder="nosaukums"><br>
+        <textarea placeholder="apraksts"></textarea><br>
+        <button type="submit">iesniegt</button>
+      </center>
   </div>
 </template>
 
@@ -14,7 +23,52 @@ export default {
   data() {
     return {
       itemsData: [],
+      new_item_box : false,
     };
   },
+  methods:{
+    new_item(){
+      this.new_item_box = true;
+      document.getElementById("site").classList.add("blur");
+    },
+    close_box(){
+      this.new_item_box = false;
+      document.getElementById("site").classList.remove("blur");
+    }
+  }
 };
 </script>
+
+<style>
+
+.new_item{
+  position: absolute;
+  background-color: #e6e1e1;
+  top: 50%;
+  left: 50%;
+  margin-top: -25vh;
+  margin-left: -25vw;
+  width: 50vw;
+  height: 50vh;
+  padding: 5px 10px 10px 15px;
+}
+
+.close_button{
+  float:right;
+}
+
+.blur{
+  filter: blur(2px);
+  pointer-events: none;
+}
+
+textarea{
+  resize: none;
+  width: 50%;
+  height: 50px;
+}
+
+.input_field{
+  width: 40%;
+}
+</style>
--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-      <h2>Items</h2>
       <ul>
-        <li v-for="item in items" :key="item.id">
-          name: {{ item.name }}<br>
-          <span v-if="item.description">Description: {{ item.description }}<br></span>
-          Deadline: {{  item.deadline }}<br>
-          <span v-if="item.completed == 0">Not Completed</span>
-          <span v-if="item.completed == 1">Completed</span>
+        <li v-for="item in filteredItems" :key="item.id">
+          <form method="PUT" :action="`http://127.0.0.1:8000/api/item/${item.id}`">
+            <h1>
+              {{ item.name }}
+              <input type="checkbox" v-model="toggle" true-value="1" false-value="false" />
+            </h1>
+  
+            <span v-if="item.description">{{ item.description }}</span>
+            <br>
+            <input type="submit" class="button update" value="update">
+          </form>
+            <button type="submit" class="button delete" @click="deleteItem(item.id)">Delete</button>test
         </li>
       </ul>
     </div>
@@ -17,31 +22,50 @@
   import axios from 'axios';
   
   export default {
-    name: 'ItemsList', // Rename the component to ItemsList
+    name: 'ItemsList',
   
     data() {
       return {
-        items: [],
+        items: []
       };
     },
-  
+    
     created() {
       axios.get('http://127.0.0.1:8000/api/items')
         .then(response => {
           this.items = response.data;
         })
         .catch(error => {
-          console.error(error);
+          console.error("no data..."+error);
         });
     },
+    
+    methods: {
+      async deleteItem(itemId) {
+        try {
+          await fetch(`http://127.0.0.1:8000/api/item/${itemId}`, { method: 'DELETE' });
+          location.reload();
+        } catch (error) {
+          console.error('Error deleting item:', error);
+        }
+      }
+    },
+    
+    computed: {
+      filteredItems() {
+        const currentDate = new Date();
+        return this.items.filter(item => {
+          const deadline = new Date(item.deadline);
+          return deadline > currentDate && item.completed !== 1;
+        });
+      }
+    }
   };
   </script>
-
-<style>
-
-li{
+  
+  <style scoped>
+  li {
     list-style: none;
-}
-
-</style>
+  }
+  </style>
   
